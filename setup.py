6451e0794eb2ef03d7a47a982d40077e758e352c
Ashwini Ugale <ashwiniugale04@gmail.com>
2025-04-10 05:42:06 +0530

    Project Update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'QApplication',
     version= '0.0.1',
-    author= 'sunny savita',
-    author_email= 'sunny.savita@gmail.com',
+    author= 'Ashwini Ugale',
+    author_email= 'ashwiniugale04@.com',
     packages= find_packages(),
     install_requires = []
 
